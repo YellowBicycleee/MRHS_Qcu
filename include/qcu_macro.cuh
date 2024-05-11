40d5a8711ba8740087a80564677eb0a2d580c029
wjc <wangjianchengcs@163.com>
2024-05-12 02:40:09 +0800

    删除部分注释，引入errorQcu宏

--- include/qcu_macro.cuh
+++ include/qcu_macro.cuh
@@ -1,27 +1,8 @@
 #pragma once
 
 
-// constexpr int Nc = 3;
+// constexpr int Nc = 3;    // Nc isn't defined to 3
 constexpr int Ns = 4;
-
-// BEGIN_NAMESPACE(qcu)
-// struct QcuDesc {
-//   int lattice_size[4];
-//   int grid_size[4];
-//   QcuDesc(int Lx, int Ly, int Lz, int Lt, int Nx, int Ny, int Nz, int Nt) {
-//     lattice_size[0] = Lx;
-//     lattice_size[1] = Ly;
-//     lattice_size[2] = Lz;
-//     lattice_size[3] = Lt;
-
-//     grid_size[0] = Nx;
-//     grid_size[1] = Ny;
-//     grid_size[2] = Nz;
-//     grid_size[3] = Nt;
-//   }
-// };
-
-// END_NAMESPACE(qcu)
 
 #define CHECK_MPI(cmd)                         \
   do {                                         \
@@ -48,4 +29,12 @@ constexpr int Ns = 4;
       fprintf(stderr, "NCCL error: %s\n", ncclGetErrorString(err)); \
       exit(1);                                                      \
     }                                                               \
+  } while (0)
+
+
+#define errorQcu(msg) \
+  do {                \
+    fprintf(stderr, msg); \
+    fprintf(stderr, "Error happened in file %s, line %d\n", __FILE__, __LINE__); \
+    exit(1);          \
   } while (0)
