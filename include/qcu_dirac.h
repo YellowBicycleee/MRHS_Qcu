24d2069d7c6c22ea6e7c0a6f8edd5a416acfc260
wjc <wangjianchengcs@163.com>
2024-05-12 02:22:22 +0800

    更新一部分dslash相关头文件

---
+++ include/qcu_dirac.h
@@ -0,0 +1,54 @@
+#pragma once
+#include "qcd/qcu_dslash.h"
+#include "qcuDesc/qcu_desc.h"
+#include "qcu_kernel_param.cuh"
+namespace qcu {
+
+class Dirac {
+ protected:
+  void* gauge_;
+  double kappa_;
+  double mass_;
+  int nColors_;
+  int nInputs_;
+  QCU_PRECISION floatPrecision_;
+  const QcuLattDesc<Nd>& lattDesc_;
+  const QcuProcDesc<Nd>& procDesc_;
+  DSLASH_TYPE dslashType_;
+
+  ExecutionStreams& streams_;
+
+ public:
+  Dirac(void* gauge, double mass, int nColors, int nInputs, QCU_PRECISION floatPrecision,
+        const QcuLattDesc<Nd>& lattDesc, const QcuProcDesc<Nd>& procDesc, DSLASH_TYPE dslashType,
+        ExecutionStreams& streams)
+      : gauge_(gauge),
+        mass_(mass),
+        kappa_(1.0 / (2.0 * (4.0 + mass))),
+        nColors_(nColors),
+        nInputs_(nInputs),
+        floatPrecision_(floatPrecision),
+        lattDesc_(lattDesc),
+        procDesc_(procDesc),
+        dslashType_(dslashType),
+        streams_(streams) {}
+
+  virtual ~Dirac() {}
+  virtual void Dslash(void* outputMRHS, void* inputMRHS, int parity, int daggerFlag) = 0;
+  virtual void DslashXpay() = 0;
+};
+
+class DiracWilson : Dirac {
+ public:
+  DiracWilson(void* gauge, double mass, int nColors, int nInputs, QCU_PRECISION floatPrecision,
+              const QcuLattDesc<Nd>& lattDesc, const QcuProcDesc<Nd>& procDesc, DSLASH_TYPE dslashType,
+              ExecutionStreams& streams)
+      : Dirac(gauge, mass, nColors, nInputs, floatPrecision, lattDesc, procDesc, dslashType, streams) {}
+  ~DiracWilson() {}
+  // TODO
+  void Dslash(void* outputMRHS, void* inputMRHS, int parity, int daggerFlag);
+  // TODO
+  void DslashXpay() {}
+};
+
+}  // namespace qcu
---
+++ include/qcu_kernel_param.cuh
@@ -0,0 +1,65 @@
+#pragma once
+
+// #include <cuda.h>
+#include <cuda_runtime.h>
+#include "qcu_macro.cuh"
+namespace qcu {
+
+struct ExecutionStreams {
+  cudaStream_t stream;
+  cudaStream_t boundaryStreams[4 * 2];  // 4 dims, 2 dirs
+
+  ExecutionStreams() {
+    CHECK_CUDA(cudaStreamCreate(&stream));
+    for (int i = 0; i < 4 * 2; i++) {
+      CHECK_CUDA(cudaStreamCreate(&boundaryStreams[i]));
+    }
+  }
+  ExecutionStreams(const ExecutionStreams &execStreams) {
+    stream = execStreams.stream;
+    for (int i = 0; i < 4 * 2; i++) {
+      boundaryStreams[i] = execStreams.boundaryStreams[i];
+    }
+  }
+  void operator=(const ExecutionStreams &execStreams) {
+    stream = execStreams.stream;
+    for (int i = 0; i < 4 * 2; i++) {
+      boundaryStreams[i] = execStreams.boundaryStreams[i];
+    }
+  }
+
+  ~ExecutionStreams() {
+    CHECK_CUDA(cudaStreamDestroy(stream));
+    for (int i = 0; i < 4 * 2; i++) {
+      CHECK_CUDA(cudaStreamDestroy(boundaryStreams[i]));
+    }
+  }
+};
+
+// struct BaseParam {
+//   static ExecutionStreams gpuStreams;
+// };
+
+// ExecutionStreams BaseParam::gpuStreams;
+
+// struct KernelParam : public BaseParam {
+//   //   int blockSize[3];
+//   //   int gridSize[3];
+//   KernelParam() {}
+//   //   {
+//   // for (int i = 0; i < 3; i++) {
+//   //   blockSize[i] = 1;
+//   //   gridSize[i] = 1;
+//   // }
+//   // blockSize[0] = 256;
+//   //   }
+//   void setParam(int blockSize, int gridSize) {}
+//   //   {  // 只用blockDim.x和gridDim.x
+//   // blockSize[0] = x;
+//   // gridSize[0] = y;
+// //   //   }
+// //   dim3 getBlockSize() const { return dim3(blockSize[0]); }  //{ return dim3(blockSize[0], blockSize[1], blockSize[2]); }
+// //   dim3 getGridSize() const {}   //{ return dim3(gridSize[0], gridSize[1], gridSize[2]); }
+// };
+
+}  // namespace qcu
