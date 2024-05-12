8ac02e5f4c8fcc6d25fca28658a65bc75aa4851b
wjc <wangjianchengcs@163.com>
2024-05-13 00:32:10 +0800

    修改DiracWilson接口和实现

--- src/qcu_dirac.cu
+++ src/qcu_dirac.cu
@@ -4,13 +4,13 @@ namespace qcu {
 
 // class DiracWilson : Dirac {
 //  public:
-void DiracWilson::Dslash(void* outputMRHS, void* inputMRHS, int parity, int daggerFlag) {
+void DiracWilson::Dslash(void* outputMRHS, void* inputMRHS, int parity, int daggerFlag, int nInputs) {
   // instantiateDslash(const QcuLattDesc<Nd>& lattDesc, const QcuProcDesc<Nd>& procDesc, int parity, int daggerFlag,
   //                      double kappa, void* mrhsFermionIn, void* mrhsFermionOut, void* gauge, DSLASH_TYPE dtype,
   //                      QCU_PRECISION floatPrecision, int nColors, int nInputs)
   DSLASH_TYPE dtype = DSLASH_WILSON;
   instantiateDslash(lattDesc_, procDesc_, parity, daggerFlag, kappa_, inputMRHS, outputMRHS, gauge_, dtype,
-                    floatPrecision_, nColors_, nInputs_, streams_);
+                    floatPrecision_, nColors_, nInputs, streams_);
 }
 
 }  // namespace qcu
