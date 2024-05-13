53a54ad853eed3307bbc58087d65fb607c3800f6
wjc <wangjianchengcs@163.com>
2024-05-14 00:15:11 +0800

    加入getDslash接口（否则会有空指针）

--- include/qcu.h
+++ include/qcu.h
@@ -20,14 +20,13 @@ void initGridSize(QcuGrid_t *grid, QcuParam *p_param, int nColors);
 void pushBackFermions(void *fermionOut, void *fermionIn);  // 押入输入输出向量
 void startDslash(int parity, int daggerFlag);
 void loadQcuGauge(void *gauge);
+void getDslash(int dslashType, void *gauge, double mass, int nColors, int nInputs, int floatPrecision);
 // void loadQcuGauge(void *gauge, QcuParam *param); // 废弃
 
-
-void dslashQcu(void *fermion_out, void *fermion_in, void *gauge, QcuParam *param, int parity); // 废弃
-void fullDslashQcu(void *fermion_out, void *fermion_in, void *gauge, QcuParam *param, int dagger_flag); // 废弃
+void dslashQcu(void *fermion_out, void *fermion_in, void *gauge, QcuParam *param, int parity);           // 废弃
+void fullDslashQcu(void *fermion_out, void *fermion_in, void *gauge, QcuParam *param, int dagger_flag);  // 废弃
 // TODO
 void cg_inverter(void *x_vector, void *b_vector, void *gauge, QcuParam *param, double p_max_prec, double p_kappa);
-
 
 #ifdef __cplusplus
 }
